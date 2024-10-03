ebffba206a5cb509adf9405cdbc2baee9ba6415b
Alex Song <mecha_alex@cloudera.com>
2024-10-03 13:44:11 -0700

    Separate agent tool backend and frontend
    Agent tools will run in cml models infra so that it is easily callable by central orchestrator

---
+++ sample_external_applications/case_summarizer_app/agent-backend/case_summarizer_backend.py
@@ -0,0 +1,31 @@
+import os
+from litellm import completion
+
+working_dir = "/home/cdsw"
+CASE_FILE_PATH_TMPL = '%s/sample_external_applications/case_summarizer_app/data/support_cases/case-%s.json'
+
+
+if os.getenv("AWS_ACCESS_KEY_ID") is None:
+    pass
+if os.getenv("AWS_SECRET_ACCESS_KEY") is None:
+    pass
+if os.getenv("AWS_REGION_NAME") is None:
+    pass
+
+def summarize_case(args):
+    case_id = args["case_id"]
+    if os.path.isfile(CASE_FILE_PATH_TMPL % (working_dir, case_id)):
+        with open(CASE_FILE_PATH_TMPL % (working_dir, case_id), 'r') as file:
+            case_data = file.read()
+    else: 
+        return '{"error": "Case file for case %s not found"}' % case_id
+    
+    messages = [
+        {"role": "system", "content": "You are a helpful and truthful support case summarization assistant. Create a very brief and concise summary of the support case represented by a json formatted file. Include the case number in the title. Include a conclusion at the end indicating the status of the case. Use markdown formatting wherever applicable."},
+        {"role": "user", "content": "%s"%case_data},
+    ]
+    response = completion(
+        model = "anthropic.claude-3-5-sonnet-20240620-v1:0",
+        messages = messages
+    )
+    return (response['choices'][0]['message']['content'], case_data)
