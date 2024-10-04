71ab9f7b8b3ecb2add17a129517bab439aa7e216
Jason Everett <jev@cloudera.com>
2024-10-04 20:45:16 +0000

    other small things

--- sample_external_applications/accounts_knowledge_graph_app/start-app-script.sh
+++ sample_external_applications/accounts_knowledge_graph_app/start-app-script.sh
@@ -5,7 +5,9 @@
 # namespace. The service will be available on some port. Also,
 # port forward the neo4j browser to CDSW_APP_PORT so it's available
 # in the UI for this application
-python sample_external_applications/accounts_knowledge_graph_app/start-neo4j.py
+
+# TODO: get neo4j server working on private cloud
+# python sample_external_applications/accounts_knowledge_graph_app/start-neo4j.py
 
 # Update the IP of the KG service so other sessions & pods can 
 # discover. TODO: this can also be a k8s service.
@@ -14,4 +16,8 @@ KG_APP_SERVICE_PORT=5678
 python sample_external_applications/accounts_knowledge_graph_app/update-kg-endpoint-info.py $KG_APP_SERVICE_IP $KG_APP_SERVICE_PORT
 
 # Serve the non-ui application endpoint. 
-fastapi dev sample_external_applications/accounts_knowledge_graph_app/serve.py --port $KG_APP_SERVICE_PORT
+nohup fastapi dev sample_external_applications/accounts_knowledge_graph_app/serve.py --port $KG_APP_SERVICE_PORT &
+
+# For now, this is a workaround hack to keep the amp long living. Ideally, we'd be able to use start-neo4j.py script
+# to port forward the neo4j browser up to CDSW_APP_PORT using standard kubernetes functions in python.
+python sample_external_applications/case_summarizer_app/agent-ui/case_summarizer_app.py
