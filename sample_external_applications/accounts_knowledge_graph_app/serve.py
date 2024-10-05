9b3df28365fdcfcebb34e3f7d12ca570a9884d30
Jason Everett <jev@Jasons-MacBook-Pro.local>
2024-10-05 10:08:45 -0400

    fix merge conflicts

--- sample_external_applications/accounts_knowledge_graph_app/serve.py
+++ sample_external_applications/accounts_knowledge_graph_app/serve.py
@@ -17,35 +17,6 @@ graph = Neo4jGraph(
 )
 
 
-from sample_external_applications.accounts_knowledge_graph_app.kg.neo4j_utils import (
-    get_neo4j_credentails,
-    is_neo4j_server_up,
-    reset_neo4j_server,
-    wait_for_neo4j_server,
-)
-
-# graph = Neo4jGraph(
-#     username=get_neo4j_credentails()["username"],
-#     password=get_neo4j_credentails()["password"],
-#     url=get_neo4j_credentails()["uri"],
-# )
-
-
-
-from sample_external_applications.accounts_knowledge_graph_app.kg.neo4j_utils import (
-    get_neo4j_credentails,
-    is_neo4j_server_up,
-    reset_neo4j_server,
-    wait_for_neo4j_server,
-)
-
-# graph = Neo4jGraph(
-#     username=get_neo4j_credentails()["username"],
-#     password=get_neo4j_credentails()["password"],
-#     url=get_neo4j_credentails()["uri"],
-# )
-
-
 app = FastAPI()
 
 
